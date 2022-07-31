3b3019a1b16095d679485c7bf6b01c93eefd8ab3
lakhyun.k <java2114@naver.com>
2022-07-31 21:39:12 +0900

    코드업 기초 100제 1011 풀이

---
+++ src/codeup100/c1011.java
@@ -0,0 +1,14 @@
+package src.codeup100;
+
+import java.util.Scanner;
+
+// [기초-입출력] 문자 1개 입력받아 그대로 출력하기(설명)
+public class c1011 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        char c = sc.next().charAt(0);
+
+        System.out.printf("%c", c);
+    }
+}
